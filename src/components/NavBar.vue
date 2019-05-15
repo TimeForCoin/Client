53d67df2e48f2ea39a95c692156fda7ab4509f49
ZhenlyChen <zhenlychen@foxmail.com>
2019-05-15 19:48:18 +0800

    Finish Login

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,9 +3,31 @@
     <div class="left-menu" @click="logoClick">
       <img src="@/assets/logo.png">Time For Coin
     </div>
-    <div class="right-menu">
-      <a-button class="menu-button" type="default" @click="signup">注册</a-button>
+    <div v-if="user === null" class="right-menu">
       <a-button class="menu-button" type="primary" @click="login">登录</a-button>
+      <a-modal
+        class="login-modal"
+        title="请在新窗口中完成登陆"
+        centered
+        :maskClosable="false"
+        :footer="null"
+        :closable="false"
+        v-model="loginModalVisible"
+        @ok="() => loginModalVisible = false"
+      >
+        <p>{{loginModalText}}</p>
+        <a-button v-if="failedLogin" class="modal-button" type="primary" @click="login">重试</a-button>
+        <a-button
+          v-if="failedLogin"
+          class="modal-button"
+          type="primary"
+          @click="()=>{loginModalVisible = false}"
+        >取消</a-button>
+      </a-modal>
+    </div>
+    <div v-if="user !== null" class="right-menu">
+      <a-avatar class="user-avatar" :src="user.info.avatar"/>
+      <span class="user-name" @click="logout">{{user.info.nickname}}</span>
     </div>
     <a-menu class="mid-menu" mode="horizontal" @click="menuClick" :style="{ lineHeight: '64px' }">
       <a-menu-item class="menu-item" key="discover">发现</a-menu-item>
@@ -15,19 +37,24 @@
 </template>
 
 <script>
+import { setInterval, clearInterval, setTimeout } from 'timers';
 export default {
-  data() {
+  data () {
     return {
+      user: null,
+      failedLogin: false,
+      loginModalText: '',
+      loginModalVisible: false,
       oldTop: 0,
       isShow: true,
       current: ['mail']
     }
   },
   methods: {
-    logoClick(event) {
+    logoClick (event) {
       this.$router.push('/')
     },
-    menuClick(event) {
+    menuClick (event) {
       switch (event.key) {
         case 'discover':
           this.$router.push('/discover')
@@ -37,11 +64,58 @@ export default {
           break
       }
     },
-    signup(event) {},
-    login(event) {
-      this.$router.push('/user')
+    async getUserInfo () {
+      try {
+        const res = await this.$service.user.GetInfo.call(this)
+        this.user = res
+      } catch (error) {
+
+      }
     },
-    onScroll() {
+    logout () {
+      this.user = null
+      this.$service.user.Logout.call(this)
+      this.$message.info('退出登陆')
+    },
+    async login (event) {
+      // this.$router.push('/user')
+      this.loginModalVisible = true
+      this.failedLogin = false
+      this.loginModalText = '等待授权中...'
+      try {
+        const res = await this.$service.user.GetVioletUrl.call(this)
+        const oauthWindow = window.open(
+          res.url,
+          'newWindow',
+          'menubar=0,scrollbars=1, resizable=1,status=1,titlebar=0,toolbar=0,location=1,width=600,height=600,top=50,left=100')
+        const timer = () => {
+          setTimeout(async () => {
+            const res = await this.$service.user.GetLoginStatus.call(this)
+            const status = res.status
+            if (status === 'violet') {
+              oauthWindow.close()
+              this.loginModalVisible = false
+              this.$message.info('登陆成功')
+              this.getUserInfo()
+            } else if (status === 'none') {
+              this.loginModalText = '登陆已取消'
+              this.failedLogin = true
+            } else if (status === 'running') {
+              timer()
+              this.loginModalText = '等待授权中...'
+            } else {
+              this.loginModalText = '登陆失败'
+              this.failedLogin = true
+            }
+          }, 1000)
+        }
+        timer()
+      } catch (error) {
+        this.$message.error('请求失败:' + error);
+        this.loginModalVisible = false
+      }
+    },
+    onScroll () {
       let top = document.scrollingElement.scrollTop
       if (this.oldTop >= top) {
         this.isShow = true
@@ -51,12 +125,14 @@ export default {
       this.oldTop = top
     }
   },
-  mounted() {
+  mounted () {
     // 屏幕滚动事件监听
     window.addEventListener('scroll', this.onScroll)
     this.oldTop = document.scrollingElement.scrollTop
+
+    this.getUserInfo()
   },
-  destroyed() {
+  destroyed () {
     window.removeEventListener('scroll', this.onScroll)
   }
 }
@@ -110,10 +186,27 @@ export default {
       font-size: 16px;
       height: 32px;
     }
+
+    .user-avatar {
+      margin-top: 20px;
+      vertical-align: bottom;
+    }
+
+    .user-name {
+      margin-left: 10px;
+      vertical-align: super;
+      cursor: pointer;
+    }
   }
 }
 
 .hide {
   opacity: 0;
+}
+.login-modal {
+  text-align: center;
+  .modal-button {
+    margin-left: 20px;
+  }
 }
 </style>
--- src/services/modules/user.js
+++ src/services/modules/user.js
@@ -1,8 +1,19 @@
-export async function GetVioletUrl(url) {
-  const res = await this.$https.get('/users/login', {
-    params: {
-      redirectUrl: url
-    }
-  })
+export async function GetVioletUrl() {
+  const res = await this.$axios.get('/session')
+  return res.data
+}
+
+export async function GetLoginStatus() {
+  const res = await this.$axios.get('/session/status')
+  return res.data
+}
+
+export async function GetInfo() {
+  const res = await this.$axios.get('/user/info/me')
+  return res.data
+}
+
+export async function Logout() {
+  const res = await this.$axios.delete('/session')
   return res.data
 }
