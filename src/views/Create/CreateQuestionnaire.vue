6a602a764dbd599f0f2097c20d4a9e640e3f3feb
T-Machine <1296939008@qq.com>
2019-06-07 02:33:45 +0800

    add file

---
+++ src/views/Create/CreateQuestionnaire.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="create-questionnaire">
+      <h1>Create questionnaire test page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.create-questionnaire {
+  margin-top: 84px;
+}
+
+</style>
